a014b84d5d35bcefe37309ab71bddd51d5a5a37c
abhishek2x <abhisheksrivastavabbn@gmail.com>
2020-08-29 02:47:50 +0530

    Updates

---
+++ precache-manifest.8afbe7787b6f2e39e8ef42afa78bf081.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eef8f230be365c1422db9039b8f21ab",
+    "url": "/abhishek2x/Calculating-in-React/index.html"
+  },
+  {
+    "revision": "6c6cb0b5853104aec467",
+    "url": "/abhishek2x/Calculating-in-React/static/css/main.a358dabf.chunk.css"
+  },
+  {
+    "revision": "7bc7bba1e1f9e16f1eed",
+    "url": "/abhishek2x/Calculating-in-React/static/js/2.a68869fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/abhishek2x/Calculating-in-React/static/js/2.a68869fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6cb0b5853104aec467",
+    "url": "/abhishek2x/Calculating-in-React/static/js/main.d167774f.chunk.js"
+  },
+  {
+    "revision": "9169ee6fef94df3695e5",
+    "url": "/abhishek2x/Calculating-in-React/static/js/runtime-main.7de445ab.js"
+  }
+]);
